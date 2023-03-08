64061e51c281dd20307de205b3fe9dca9555381b
Vy Nguyen <vy@vymac.local>
2023-03-08 18:56:17 -0500

    update results

--- main.py
+++ main.py
@@ -1,130 +1,62 @@
-import os
-import re
-from typing import List
-
 import streamlit as st
+from utils import *
 
 
-def read_file(path: str) -> List[str]:
+def look_up_episode(query: str, folder_path: str) -> List[str]:
     """
 
-    :param path: e.g., "datasets/season1/episode1.srt"
-    :return:  ["CAR HORNS HONKING",
-            "There's nothing to tell. It's just some guy I work with.", ....]
+    :param query:
+    :param folder_path:
+    :return:
     """
-    # Open the file in read mode
-    with open(path, 'r', encoding="latin-1") as file:
-        # Read the contents of the file
-        content = file.read()
-
-    # Print the contents of the file
-    content = content.split("\n\n")  # split content into paragraphs
-    content = [x[2:] for x in content]  # remove the first number in each
-    pattern = r'(?<=[a-zA-Z,])\n'  # combine into a sentence if not been a sentence yet
-    for i in range(len(content)):
-        content[i] = re.sub(pattern, ' ', content[i])
-
-    return content
-
-
-def process_text(text: str):
-    clean_text = text.lower()
-    remove_characters = [",", "?", "!", ".", "-", "_"]
-    replacements = {
-        "don't": "do not",
-        "I'd": "I would",
-        "it's": "it is",
-        "'ve": "have",
-        "there's": "there is",
-        "we're": "we were",
-        "i'm": "i am",
-        "won't": "will not",
-        "can't": "cannot",
-        "'s": " is",
-        "how's it going": "how is it going",
-        "'re": " are",
-        "how you doin": "how you doing",
-        "how're you doin": "how are you doing"
-    }
-    for contraction, expanded in replacements.items():
-        clean_text = clean_text.replace(contraction, expanded)
-
-    for c in remove_characters:
-        clean_text = clean_text.replace(c, "")
-    return clean_text
-
-
-def find_text_in_file(query: str, text_file: List[str]) -> bool:
-    query = process_text(query)
-    for text in text_file:
-        text = process_text(text)  # e.g., i don't want her to go through what i went through with carl.
-        if query in text:
-            return True
-    return False
-
-
-def get_all_file_paths(folder_path: str) -> List[str]:
-    """
-
-    :param folder_path: "datasets"
-    :return: ["datasets/season1/friends.s01e01.720p.bluray.x264--sujaidr.srt",
-            "datasets/season1/friends.s01e02.720p.bluray.x264--sujaidr.srt",
-            ....
-            "datasets/season2/friends.s02e01.720p.bluray.x264--sujaidr.srt",
-            ...
-            "datasets/season10/friends.s10e01.720p.bluray.x264--sujaidr.srt"]
-    """
-
-    file_path_list = []
-    # Walk through the folder and its subdirectories using os.walk()
-    for root, dirs, files in os.walk(folder_path):
-        # Iterate through all files in the current directory
-        for filename in files:
-            # Join the root path and filename to get the full file path
-            file_path = os.path.join(root, filename)
-            if ".srt" in file_path:
-                file_path_list.append(file_path)
-    return file_path_list
-
-
-def look_up_episode(query: str) -> List[str]:
-    folder_path = "datasets"
     res = []
     file_path_list = get_all_file_paths(folder_path)
     for file_path in file_path_list:
-        text_file = read_file(file_path)
-        is_found = find_text_in_file(query, text_file)
-        if is_found:
-            res.append(file_path)
+        text_list = read_file(file_path)  # all contents in an episode
+        found_text = find_text_in_file(query, text_list)
+        if found_text != "":
+            cleaned_result = format_result(file_path)
+            res.append(cleaned_result + "\n" +found_text)
     return res
 
 
-def format_result(file_path_list: List[str]) -> List[str]:
-    result = []
-    for file_path in file_path_list:
-        split_file_path = file_path.split("/")
-        result.append(split_file_path)
-    return result
+def format_result(result: str) -> List[str]:
+    """
+
+    :param result: a file path
+    :return: list of results, which include which season and episode
+    """
+    remove_left_bracket = result.replace("[", "")
+    remove_right_bracket = remove_left_bracket.replace("]", "")
+    split_result = remove_right_bracket.split("/")
+    split_result = split_result[2].split("-")
+    season_episode = split_result[1].strip().split("x")
+    episode_title = split_result[-1].replace(".srt","").replace(".en","")
+    final_result = f"Season {season_episode[0]}, Episode {season_episode[1]}, Title:{episode_title}"
+
+    return final_result
 
 
-def run_web():
+
+def run_web(folder_path: str):
     st.write(
         """
         # Friends TV Show Episode Lookup
         """
     )
-    st.image("img/friends_cover.jpeg", width=700)
+    st.image("img/friends_cover.jpeg", width=900)
 
-    query = st.text_input("Enter the text to look up, for example 'how're you doing?'", "")
+    query = st.text_input('Enter the text to look up, for example "how\'re you doing?"', "")
+    # st.selectbox("who spoke?", ["Joe", "Chandler", "Monica"], )
     if query != "":
-        res = look_up_episode(query)
-        st.write(res)
+        res = look_up_episode(query, folder_path)
+        for r in res:
+            st.write(r)
 
 
 if __name__ == '__main__':
-    run_web()
+    run_web(folder_path="datasets")
 
-    # list_name = ["datasets/Season_10/Friends - [10x11] - The One Where the Stripper Cries - [arsenaloyal].srt",
-    #              "datasets/Season_8/Friends - [8x19] - The One With Joey's Interview - [arsenaloyal].srt"]
-    # season_name = format_result(list_name)
-    # print(season_name)
+    res = "datasets/Season_10/Friends - [10x11] - The One Where the Stripper Cries.srt"
+    season_name = format_result(res)
+    print(season_name)
---
+++ utils.py
@@ -0,0 +1,90 @@
+import os
+import re
+from typing import List
+
+
+def read_file(path: str) -> List[str]:
+    """
+
+    :param path: e.g., "datasets/season1/episode1.srt"
+    :return:  ["CAR HORNS HONKING",
+            "There's nothing to tell. It's just some guy I work with.", ....]
+    """
+    # Open the file in read mode
+    with open(path, 'r', encoding="latin-1") as file:
+        # Read the contents of the file
+        content = file.read()
+
+    # Print the contents of the file
+    content = content.split("\n\n")  # split content into paragraphs
+    content = [x[2:] for x in content]  # remove the first number in each
+    pattern = r'(?<=[a-zA-Z,])\n'  # combine into a sentence if not been a sentence yet
+    for i in range(len(content)):
+        content[i] = re.sub(pattern, ' ', content[i])
+
+    return content
+
+
+def process_text(text: str):
+    clean_text = text.lower()
+    remove_characters = [",", "?", "!", ".", "-", "_"]  # remove these from the text
+    replacements = {
+        "don't": "do not",
+        "I'd": "I would",
+        "we'd": "we would",
+        "it's": "it is",
+        "'ve": "have",
+        "there's": "there is",
+        "we're": "we were",
+        "i'm": "i am",
+        "won't": "will not",
+        "can't": "cannot",
+        "'s": " is",
+        "how's it going": "how is it going",
+        "'re": " are",
+        "how you doin": "how you doing",
+        "how're you doin": "how are you doing"
+    }
+    for contraction, expanded in replacements.items():
+        clean_text = clean_text.replace(contraction, expanded)
+
+    for c in remove_characters:
+        clean_text = clean_text.replace(c, "")
+    return clean_text
+
+
+def find_text_in_file(query: str, text_file: List[str]) -> str:
+    query = process_text(query)
+    for text in text_file:
+        text = process_text(text)  # e.g., i don't want her to go through what i went through with carl.
+        if query in text:
+            return text
+    return ""
+
+
+def get_all_file_paths(folder_path: str) -> List[str]:
+    """
+
+    :param folder_path: "datasets"
+    :return: ["datasets/season1/friends.s01e01.720p.bluray.x264--sujaidr.srt",
+            "datasets/season1/friends.s01e02.720p.bluray.x264--sujaidr.srt",
+            ....
+            "datasets/season2/friends.s02e01.720p.bluray.x264--sujaidr.srt",
+            ...
+            "datasets/season10/friends.s10e01.720p.bluray.x264--sujaidr.srt"]
+    """
+
+    file_path_list = []
+    # Walk through the folder and its subdirectories using os.walk()
+    for root, dirs, files in os.walk(folder_path):
+        # Iterate through all files in the current directory
+        for filename in files:
+            # Join the root path and filename to get the full file path
+            file_path = os.path.join(root, filename)
+            if ".srt" in file_path:
+                file_path_list.append(file_path)
+    return file_path_list
+
+if __name__ == '__main__':
+    content = read_file("datasets/Season_1/Friends - [1x03] - The One with the Thumb.srt")
+    print(content)
