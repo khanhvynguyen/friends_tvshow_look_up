b05d1ee648a66ab4b09f0b5a1d8f2daa136dcd87
Vy Nguyen <vy@vymac.local>
2023-03-16 21:11:01 -0400

    fix format

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 import streamlit as st
 from utils import *
 
 
-def look_up_episode(query: str, folder_path: str) -> List[str]:
+def look_up_episode(query: str, folder_path: str) -> List[Dict]:
     """
-
     :param query:
     :param folder_path:
     :return:
@@ -21,17 +20,18 @@ def look_up_episode(query: str, folder_path: str) -> List[str]:
             searching_res.append((season, episode, clean_title, text_found))
 
     searching_res = sorted(searching_res)
-    res = []
+    res_list = []
     for x in searching_res:
-        r = {"title": x[-2],
-               "text_found": x[-1]}
-        res.append(r)
-    return res
+        res = {
+            "title": x[-2],
+            "text_found": x[-1]
+        }
+        res_list.append(res)
+    return res_list
 
 
 def format_title(result: str) -> Tuple:
     """
-
     :param result: a file path
     :return: list of results, which include which season and episode
     """
@@ -40,11 +40,10 @@ def format_title(result: str) -> Tuple:
     split_result = remove_right_bracket.split("/")
     split_result = split_result[2].split("-")
     season, episode = split_result[1].strip().split("x")
-    episode_title = split_result[-1].replace(".srt","").replace(".en","").replace(".sub","")
+    episode_title = split_result[-1].replace(".srt", "").replace(".en", "").replace(".sub", "")
     clean_title = f"Season {season}, Episode {episode}, Title:{episode_title}"
 
     return int(season), int(episode), clean_title
-
 
 
 def run_web(folder_path: str):
@@ -52,24 +51,23 @@ def run_web(folder_path: str):
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
     st.write(
-       "<h2>Friends TV Show - Episode Lookup </h2>", unsafe_allow_html=True)
+        "<h2>Friends TV Show - Episode Lookup </h2>", unsafe_allow_html=True)
     st.image("img/friends_cover.jpeg", width=900)
 
     query = st.text_input('Enter the text to look up, for example "how\'re you doing?"', "")
     # st.selectbox("who spoke?", ["Joe", "Chandler", "Monica"], )
     if query != "":
-        res = look_up_episode(query, folder_path)
-        for r in res:
+        res_list = look_up_episode(query, folder_path)
+        for res in res_list:
             st.write(
-                     f"""<p style="font-size:26px; color: rgb(17, 134, 145);"><i>{r["title"]}</i></p>{r["text_found"]}
+                f"""<p style="font-size:26px; color: rgb(17, 134, 145);"><i>{res["title"]}</i></p>{res["text_found"]}
                      <br> </br>
                      """,
-                     unsafe_allow_html=True)
+                unsafe_allow_html=True)
 
 
 if __name__ == '__main__':
     run_web(folder_path="datasets")
 
-    ## Debug
+    # Debug
     # look_up_episode("really white", folder_path="datasets")
-
